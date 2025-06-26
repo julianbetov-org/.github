8c8736e158e9a041edbdb7fcb9cb111a1b897cd3
Julian Bermudez <julianbetov@gmail.com>
2025-06-26 12:55:26 -0500

    Add validation methods to user class

--- src/main/java/user.java
+++ src/main/java/user.java
@@ -1,5 +1,13 @@
 public class user {
 
     private String name;
-    private String name;
+
+    public boolean firstMethod(String name) {
+        return name != null && !name.isEmpty();
+    }
+
+    public boolean secondMethod(String name) {
+        return name != null && !name.isEmpty();
+    }
+
 }
